746c6ff8668b8f881ba082ff988bba0cd9682368
Rutvikraj Champavat <rrchampavat1110@gmail.com>
2024-07-04 17:00:50 +0530

    FEAT: Different delimiters support test case completed

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,9 +1,18 @@
 const add = (numbers: string): number => {
   if (numbers === "") return 0;
 
-  const nums = numbers.split(/[,\n]/).map(Number);
+  let delimiter = /[,\n]/;
+
+  if (numbers.startsWith("//")) {
+    const [delimiterPart, numbersPart] = numbers.split("\n");
+
+    delimiter = new RegExp(delimiterPart.slice(2));
+
+    numbers = numbersPart;
+  }
+
+  const nums = numbers.split(delimiter).map(Number);
 
   return nums.reduce((sum, num) => sum + num, 0);
 };
-
 export default add;
