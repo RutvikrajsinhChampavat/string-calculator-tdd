79e374c3be420bd97f9917c96299870756e4d45a
Rutvikraj Champavat <rrchampavat1110@gmail.com>
2024-07-04 16:34:01 +0530

    FEAT: Completed add function

---
+++ stringCalculator.ts
@@ -0,0 +1,9 @@
+const add = (numbers: string): number => {
+  if (numbers === "") return 0;
+
+  const nums = numbers.split(",").map(Number);
+
+  return nums.reduce((sum, num) => sum + num, 0);
+};
+
+export default add;
